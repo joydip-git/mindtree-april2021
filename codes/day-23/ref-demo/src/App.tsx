2477c0278b863682cdbf7736a74c38e256be0375
joydip-git <34733254+joydip-git@users.noreply.github.com>
2021-06-30 11:30:19 +0530

    day-23 code uploaded

---
+++ codes/day-23/ref-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DataForm from './DataForm';
+
+function App() {
+  return (
+    <div className="App">
+      <DataForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ codes/day-23/ref-demo/src/DataForm.tsx
@@ -0,0 +1,47 @@
+import React, { Component, createRef } from 'react'
+
+export default class DataForm extends Component {
+    state = {
+        data: 10
+    }
+    inputElementRef = createRef<HTMLInputElement>()
+    /*
+    inputRef: HTMLInputElement | undefined;
+    setInputRefCallback = (elementRef: HTMLInputElement) => {
+        this.inputRef = elementRef;
+        //this.putFocus()
+        //console.log(this.inputRef ? this.inputRef : null);
+    }
+    */
+    call = (ev: any) => { console.log(ev.target) }
+
+    putFocus = () => {
+        //console.log(this.inputRef);
+        // if (this.inputRef) {
+        //     this.inputRef.focus()
+        // }
+        //this.inputRef?.focus()
+    }
+
+    validate = (ev: any) => {
+        //console.log(ev.target === this.inputRef);
+    }
+
+    render() {
+        console.log('Form renderded');
+        return (
+            <div onClick={this.call}>
+                Data:&nbsp;&nbsp;{this.state.data}
+                <br />
+                {/* <input type='text' ref={this.setInputRefCallback} onChange={this.validate} /> */}
+                <input type='text' ref={this.inputElementRef} onChange={this.validate} />
+            </div>
+        )
+    }
+    componentDidMount() {
+        console.log(this.inputElementRef);
+        console.log(this.inputElementRef.current);
+        this.inputElementRef.current?.focus()
+        console.log('Form mounted');
+    }
+}
