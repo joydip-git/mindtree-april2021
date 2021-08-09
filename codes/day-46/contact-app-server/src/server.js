b348238f8c4decfe88aff88fe8fd86cb89faec40
joydip mondal <joydip.mondal@gmail.com>
2021-08-09 16:31:33 +0530

    MCQ uploaded

--- codes/day-46/contact-app-server/src/server.js
+++ codes/day-46/contact-app-server/src/server.js
@@ -3,9 +3,14 @@ const { json } = require('body-parser')
 const cors = require('cors')
 const { routerMiddleware } = require('./routes/app-routes')
 const { connectdb } = require('./db/db')
+const dotenv = require('dotenv')
+dotenv.config()
 
-
-const PORT = 3001
+// const args = process.argv.splice(2)
+// console.log(args)
+// console.log(process.env.PORT)
+// console.log(process.env.NAME)
+const PORT = process.env.PORT
 
 const app = express()
 
